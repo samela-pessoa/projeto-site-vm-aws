9b1afb7f41b3cf347e816c7570ec96032dffe8d2
soliveira024 <soliveira024@hotmail.com>
2024-12-15 15:23:27 -0300

    alterando s3

--- terraform-infrastructure/s3-backed/dev/main.tf
+++ terraform-infrastructure/s3-backed/dev/main.tf
@@ -1,5 +1,6 @@
 module "s3_backend" {
   source  = "git::https://github.com/samela-pessoa/terraform-modules-aws.git//aws_s3_bucket?ref=feature/dev"
+  state_key = var.state_key
   project = var.project
   region  = var.region
   env     = var.env
