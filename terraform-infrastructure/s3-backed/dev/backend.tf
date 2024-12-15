b43c34e947882bd460da2cd2c38c3e9c4e982117
soliveira024 <soliveira024@hotmail.com>
2024-12-15 13:48:04 -0300

    alterando s3

--- terraform-infrastructure/s3-backed/dev/backend.tf
+++ terraform-infrastructure/s3-backed/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "s3-backend-your-project" # Substitua pelo nome do bucket
+    bucket         = "s3-backend-terraform-ue1-dev-182" # Substitua pelo nome do bucket
     key            = "terraform-state/default.tfstate" # Será substituído no terraform init
     region         = "us-east-1"
     encrypt        = true
